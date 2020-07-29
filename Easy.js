f370a4b691ab8c73bf47e3784f38568c6ea57018
Oneil <oneilswilliams@gmail.com>
2020-07-28 20:46:46 -0400

    subarray

--- Easy.js
+++ Easy.js
@@ -95,3 +95,33 @@ function fibonacci(num, memo) {
 
   return memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo);
 }
+
+const maxSubArray = (nums) => {
+    
+  let sum = nums[0]    
+  for(let i = 0; i < nums.length; i++){
+      let currentSum = nums[i]
+      if(currentSum > sum) {
+          sum = currentSum       
+      }
+      for(let j = i+1; j < nums.length; j++){
+          currentSum += nums[j]
+          if(currentSum > sum) {
+              sum = currentSum       
+          }
+      }
+  }
+  return sum
+};
+
+// Kadane
+
+const maxSubArray = (nums) => {
+  let current = nums[0]
+  let max = nums[0]
+  for(let i = 1; i < nums.length; i++){
+      current = Math.max(current + nums[i], nums[i])
+      max = Math.max(current, max)
+  }
+  return max
+}
